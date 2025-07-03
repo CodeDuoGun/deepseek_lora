71be3d8198938c077372cfc2778168aeea9e2c61
jocelyn <tangxueduo@sihui.email>
2025-07-03 10:15:39 +0800

    [FIX]微调bugs

--- README.md
+++ README.md
@@ -1,16 +1,30 @@
 # Introduction
+supported features
+- [x] deepseek lora sft 微调多轮对话
+- [ ] qwen3 lora sft 微调ai病历, 开发中
 
-# Install
+
+# ENV Install
 ```bash
 conda create -n lora python=3.10
 conda activate lora
 pip install -r requirements.txt
 ```
 
-# Run conv
+# Dataset prepare
+## TCM data
+
+## conversation data
+
+# Conv finetune
+
+## run
 ```bash
 python finetune-multi-conv.py
 ```
+
+# Instruct finetune
+
 
 # Run openmind
 ```bash
---
+++ medical/merge_lora_model.py
@@ -0,0 +1,26 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from peft import PeftModel
+
+
+
+def merge_model():
+    base_model_name = "Qwen/Qwen3-8B"  # 原始模型名
+    lora_path = "medical/output/qwen3_lora"  # LoRA adapter 输出路径
+
+    # 1. 加载基础模型
+    model = AutoModelForCausalLM.from_pretrained(base_model_name, device_map="auto", trust_remote_code=True)
+
+    # 2. 加载 LoRA 权重
+    model = PeftModel.from_pretrained(model, lora_path)
+
+    # 3. 合并 LoRA → 原始模型
+    model = model.merge_and_unload()  # 🔥 核心一步：LoRA 融合
+
+    # 2. 保存为新的 Huggingface 格式权重（合并后）,现在这个模型就不再依赖 LoRA Adapter，也不再依赖 peft 库加载，可以独立用于推理、部署到 WebUI 或 llama.cpp 之类环境。
+    # 保存为 safetensors（部署更快更安全）, model.safetensors
+    model.save_pretrained("medical/model/qwen3_8b_merged", safe_serialization=True)
+    tokenizer = AutoTokenizer.from_pretrained(base_model_name, trust_remote_code=True)
+    tokenizer.save_pretrained("medical/model/qwen3_8b_merged")
+    
+if __name__=="__main__":
+    merge_model()
